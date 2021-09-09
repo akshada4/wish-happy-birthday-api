c299689ca970387244aaf7b42c5f7709eda587ce
akshada4 <akshadasipsap@gmail.com>
2021-09-10 00:07:09 +0530

    changed package

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ const client = require('twilio')(accountSid,authToken);
 const upload = multer();
 const app = express();
 
-const sendMessage = (req,res) => {
-	const number = process.env.TWILIO_NUMBER;
+const sendMessage = (req,res,next) => {
 	const messagingSid = process.env.MESSAGING_SID;
 	const sender = req.body.sender;
 	const celebrant = req.body.celebrant;
-	const celebrantNumber = req.body.number;
+	const number = req.body.number;
 
 	const message = `${sender} says: Happy birthday, ${celebrant}`;
 
@@ -22,13 +21,14 @@ const sendMessage = (req,res) => {
 				.create({
 					body: message,  
          	messagingServiceSid: messagingSid,      
-         	to: celebrantNumber
+         	to: number
 				})
 				.done();
+	next();
 }
 
 
-app.post("/", upload.none(), sendMessage)
+app.post("/", upload.none(), sendMessage, (req,res) => res.status(200).send())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
