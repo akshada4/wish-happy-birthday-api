7375f24cf00f05b159effb1fdfc408f09c650d4c
akshada4 <akshadasipsap@gmail.com>
2021-09-09 23:46:49 +0530

    y do i feel like http wont work

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const multer = require('multer');
+
+require('dotenv').config();
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid,authToken);
+
 const upload = multer();
 const app = express();
 
+const sendMessage = (req,res) => {
+	const number = process.env.TWILIO_NUMBER;
+	const messagingSid = process.env.MESSAGING_SID;
+	const sender = req.body.sender;
+	const celebrant = req.body.celebrant;
+	const celebrantNumber = req.body.number;
 
-app.post("/", upload.none(), (req,res) => console.log(req.body.sender))
+	const message = `${sender} says: Happy birthday, ${celebrant}`;
+
+	client.messages
+				.create({
+					body: message,  
+         	messagingServiceSid: messagingSid,      
+         	to: celebrantNumber
+				})
+				.done();
+}
+
+
+app.post("/", upload.none(), sendMessage)
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}...`))
+app.listen(port, () => console.log(`listening on port ${port}...`));
